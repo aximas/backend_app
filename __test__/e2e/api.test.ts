b6f97a29e5e1a2ab568ae621f43d2447581eb5cf
aximas <northdakota778@gmail.com>
2022-08-27 01:11:34 +0500

    NEW: api test; FIX: some fixes with variables in index.ts

---
+++ __test__/e2e/api.test.ts
@@ -0,0 +1,70 @@
+import request from 'supertest';
+import {app, HTTP_STATUSES} from '../../src';
+import {STATUS_CODES} from 'http';
+
+describe('/test api', () => {
+    beforeAll(async () => {
+        await request(app).delete('/__test__/data');
+    });
+
+    it('should return 200 and empty array', async () => {
+        await request(app).get('/users').expect(HTTP_STATUSES.OK_200, []);
+    });
+
+    it('should return 404 for not existing user', async () => {
+        await request(app).get('/users/Valibek').expect(HTTP_STATUSES.NOT_FOUND_404);
+    });
+
+    it('should not create user with incorrect input data', async () => {
+        await request(app).post('/user').send({name: ''}).expect(HTTP_STATUSES.BAD_REQUEST_400);
+        await request(app).get('/users').expect(HTTP_STATUSES.OK_200, []);
+    });
+
+    let _createdUser: {  id: number, name: string } = {id: 0, name: ''};
+    it('should create user with correct input data', async () => {
+        const createResponse = await request(app).post('/user').send({name: 'Alex'}).expect(HTTP_STATUSES.CREATED_201);
+        _createdUser = createResponse.body;
+
+        expect(_createdUser).toEqual({
+            id: expect.any(Number),
+            name: 'Alex'
+        });
+        await request(app).get('/users').expect(HTTP_STATUSES.OK_200, [_createdUser]);
+    });
+
+    it('shouldn\'t not update user with incorrect input data', async () => {
+        await request(app).put(`/user/212`).send({name: ''}).expect(HTTP_STATUSES.BAD_REQUEST_400);
+        await request(app).get(`/user/${_createdUser.id}`).expect(HTTP_STATUSES.OK_200, [_createdUser]);
+    });
+
+    it('should update user with correct input data', async () => {
+        const createResponse = await request(app).put(`/user/${_createdUser.id}`).send({name: 'Alexey'}).expect(HTTP_STATUSES.CREATED_201);
+        const changedUser = createResponse.body;
+
+        expect(changedUser).toEqual({
+            id: expect.any(Number),
+            name: 'Alexey'
+        });
+        await request(app).get('/users').expect(HTTP_STATUSES.OK_200, [changedUser]);
+    });
+
+    it('shouldn\'t not update user that not exist', async () => {
+        await request(app).put(`/user/212`).send({name: 'Giyas'}).expect(HTTP_STATUSES.NOT_FOUND_404);
+    });
+
+    it('should update user with correct input data and get with user', async () => {
+        const createResponse = await request(app).put(`/user/${_createdUser.id}`).send({name: 'Johnson'}).expect(HTTP_STATUSES.CREATED_201);
+        const changedUser = createResponse.body;
+
+        expect(changedUser).toEqual({
+            id: expect.any(Number),
+            name: 'Johnson'
+        });
+        await request(app).get(`/user/${_createdUser.id}`).expect(HTTP_STATUSES.OK_200, [changedUser]);
+    });
+
+    it('should delete user with correct input data', async () => {
+        await request(app).delete(`/user/${_createdUser.id}`).expect(HTTP_STATUSES.NO_CONTENT_204);
+        await request(app).get(`/users`).expect(HTTP_STATUSES.OK_200, []);
+    });
+});
--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,86 @@
 import express from 'express';
 
-const app = express()
-const port = 3000
+export const app = express();
+const port = 4000;
 
-app.get('/', (req, res) => {
-    res.send('Ravshan !s1')
-})
+export const HTTP_STATUSES = {
+    OK_200: 200,
+    CREATED_201: 201,
+    NO_CONTENT_204: 204,
+
+    BAD_REQUEST_400: 400,
+    NOT_FOUND_404: 404
+};
+
+const JsonBodyMiddleware = express.json();
+app.use(JsonBodyMiddleware);
+
+const db = {
+    users: [
+        {id: 1, name: 'Rava'},
+        {id: 2, name: 'Anvar'},
+        {id: 3, name: 'Ruslan'},
+        {id: 4, name: 'Rahmatillo'}
+    ]
+};
+
+app.get('/users', (req, res) => {
+
+    res.status(HTTP_STATUSES.OK_200).json(db.users);
+});
+
+app.get('/user/:id', (req, res) => {
+    const userId = +req.params.id;
+
+    if (!userId) res.status(HTTP_STATUSES.NOT_FOUND_404).json({error: 'user id not found as uri parameter'});
+
+    const foundUsers = db.users.filter(u => u.id === userId);
+
+    res.status(HTTP_STATUSES.OK_200).json(foundUsers);
+});
+
+app.post('/user', async (req, res) => {
+    const userName = req.body.name;
+    if (!userName) return res.status(HTTP_STATUSES.BAD_REQUEST_400).json({error: 'name in body is not exited'});
+
+    const createdUser = {
+        id: +(new Date()),
+        name: userName
+    };
+    db.users.push(createdUser);
+    res.status(HTTP_STATUSES.CREATED_201).json(createdUser);
+});
+
+app.put('/user/:id', (req, res) => {
+    const userName = req.body.name;
+    const userId = +req.params.id;
+
+    if (!userName) res.status(HTTP_STATUSES.BAD_REQUEST_400).json({error: 'name parameter in body is not exist'});
+
+    const foundUser = db.users.find(user => user.id === userId);
+
+    if (!foundUser) res.status(HTTP_STATUSES.NOT_FOUND_404).json({error: 'user with id in uri not found'});
+
+    foundUser!.name = userName;
+
+    res.status(HTTP_STATUSES.CREATED_201).json(foundUser);
+});
+
+app.delete('/user/:id', (req, res) => {
+    const userId = req.params.id;
+
+    if (!userId) res.status(HTTP_STATUSES.BAD_REQUEST_400).json({error: 'unknown uri parameter'});
+
+    db.users = db.users.filter(user => user.id !== +userId);
+
+    res.sendStatus(HTTP_STATUSES.NO_CONTENT_204);
+});
+
+app.delete('/__test__/data', (req, res) => {
+    db.users = [];
+    res.sendStatus(HTTP_STATUSES.NO_CONTENT_204);
+});
 
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
-})
+    console.log(`Example app listening on port ${port}`);
+});
